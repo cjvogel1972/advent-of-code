8440b9756a74f594c775960726aef17ed331e038
Chris Vogel <cjvogel1972@gmail.com>
2025-12-03 11:00:01 -0600

    Solution for Day 3 2025
    Relatively easy one. Started with brute force for part 1, but then
    realized I needed a better way for part 2.

--- 2025/day03/day.py
+++ 2025/day03/day.py
@@ -1,6 +1,43 @@
 def solve_part1(lines: list[str]) -> int:
-    return 0
+    total_joltage = 0
+
+    for line in lines:
+        total_joltage += max_joltage_for_bank(line, 2)
+
+    return total_joltage
 
 
 def solve_part2(lines: list[str]) -> int:
-    return 0
+    total_joltage = 0
+
+    for line in lines:
+        total_joltage += max_joltage_for_bank(line, 12)
+
+    return total_joltage
+
+
+def max_joltage_for_bank(battery_bank: str, num_batteries: int) -> int:
+    max_joltage = ""
+    start = 0
+
+    for i in range(num_batteries):
+        end = num_batteries - i - 1
+        bank = battery_bank[start:-end] if end > 0 else battery_bank[start:]
+        battery, slot = largest_battery(bank)
+        start += slot + 1
+        max_joltage += battery
+
+    return int(max_joltage)
+
+
+def largest_battery(battery_bank: str) -> tuple[str, int]:
+    battery_joltage = 0
+    slot = 0
+    
+    for i in range(len(battery_bank)):
+        joltage = int(battery_bank[i])
+        if joltage > battery_joltage:
+            battery_joltage = joltage
+            slot = i
+
+    return battery_bank[slot], slot
--- 2025/day03/test_day.py
+++ 2025/day03/test_day.py
@@ -7,14 +7,20 @@ class Test(unittest.TestCase):
 
     def test_solve_part1(self):
         lines = [
-            "",
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
         ]
 
-        self.assertEqual(0, day.solve_part1(lines))
+        self.assertEqual(357, day.solve_part1(lines))
 
     def test_solve_part2(self):
         lines = [
-            "",
+            "987654321111111",
+            "811111111111119",
+            "234234234234278",
+            "818181911112111",
         ]
 
-        self.assertEqual(0, day.solve_part2(lines))
+        self.assertEqual(3121910778619, day.solve_part2(lines))
